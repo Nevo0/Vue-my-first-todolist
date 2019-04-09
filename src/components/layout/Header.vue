6e4f49971570f461cf9763f6588f6415d56d55f7
Nevo0 <Piecyk.rafal@gmail.com>
2019-04-09 15:25:59 +0200

    Todo App

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1>Todo List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+ <style scoped>
+.header {
+  text-align: center;
+  background: #333;
+  color: #fff;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
+ 
